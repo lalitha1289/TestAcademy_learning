60d4f6d679900cae2e852d85ba7c79439e2017e0
lalitha <kolleparalalitha1289@gmail.com>
2025-06-05 07:56:55 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Hello_world.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Hello_world {
+    public static  void main(String args[])
+    {
+        System.out.println("Helloworld this is my basic class");
+    }
+}
